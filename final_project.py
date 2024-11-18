ee7465a26634f4452996e22e1cc5e18f8a08c33b
seamus-stirling <sstirling@falcon.bentley.edu>
2024-11-18 18:38:58 -0500

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -94,7 +94,7 @@ def city_dataframe(city_restaurant_data, city_selector):
 # [ST1] need to add ST 2-4
 def home_page(city_restaurant_data, cities, restaurant_data):
     st.set_page_config(layout="wide")
-    "Fast Food Restaurants by City"
+    st.title("Fast Food Restaurants by City")
     popularity_map(restaurant_data)
     city_selector = st.selectbox("Select a City", cities)
     column1, column2 = st.columns(2)
