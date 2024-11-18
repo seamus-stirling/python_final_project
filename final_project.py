f542973c1302cbdefa1cd306fe4d74050446fdcf
seamus-stirling <sstirling@falcon.bentley.edu>
2024-11-18 12:43:12 -0500

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -16,11 +16,8 @@ def city_splitter(restaurant_data):
     city_restaurant_data = {city: restaurant_data[restaurant_data['city'] == city] for city in cities}
     return city_restaurant_data, cities
 
-def city_maps(city_restaurant_data, cities):
-    st.header("Fast Food Restaurants by City")
-    city_selector = st.selectbox("Select a City", cities)
+def city_maps(city_restaurant_data, city_selector):
     st.write("Current City:", city_selector)
-
     selected_city = city_selector
     view_state = pdk.ViewState(
         latitude = float(city_restaurant_data[selected_city]["latitude"].mean()),
@@ -43,8 +40,34 @@ def city_maps(city_restaurant_data, cities):
         layers = layer
     )
     st.pydeck_chart(map)
-
     return
+
+def city_dataframe(city_restaurant_data, city_selector):
+    st.dataframe(
+        city_restaurant_data[city_selector],
+        column_config={
+            "categories": "Restaurant Type",
+            "name": "Name",
+            "address": "Street",
+            "city": "City",
+            "province": "State",
+            "postalCode": "Zip Code",
+            "latitude":"Latitude",
+            "longitude": "Longitude"
+        }
+    )
+
+def home_page(city_restaurant_data, cities):
+    st.header("Fast Food Restaurants by City")
+    city_selector = st.selectbox("Select a City", cities)
+    column1, column2 = st.columns(2)
+    with column1:
+        st.header = "Interactive Map"
+        city_maps(city_restaurant_data, city_selector)
+    with column2:
+        st.header = "Restaurant Locations"
+        city_dataframe(city_restaurant_data, city_selector)
+
 
 def most_locations():
 
@@ -62,5 +85,5 @@ def main():
     csv_file = "fast_food_usa.csv"
     restaurant_data = read_data(csv_file)
     city_restaurant_data, cities = city_splitter(restaurant_data)
-    city_maps(city_restaurant_data, cities)
+    home_page(city_restaurant_data, cities)
 main()
